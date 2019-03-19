57bd0ff46499e7b7856db3edbba5c050d85c4b52
Mateusz Iwańczuk <iwanczukmateusz@gmail.com>
2019-03-19 12:17:11 +0100

    Updates

---
+++ precache-manifest.8aa579f4bfae43d9d21d3eaf99d41c1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f64071c318b3c09e8dbb",
+    "url": "/countries/static/css/main.96901d9c.chunk.css"
+  },
+  {
+    "revision": "f64071c318b3c09e8dbb",
+    "url": "/countries/static/js/main.f64071c3.chunk.js"
+  },
+  {
+    "revision": "2dfaedb93624728921e3",
+    "url": "/countries/static/js/runtime~main.2dfaedb9.js"
+  },
+  {
+    "revision": "671b4c136bc7cbba354b",
+    "url": "/countries/static/js/2.671b4c13.chunk.js"
+  },
+  {
+    "revision": "4d900b0e68db2c1bf51453225b51086c",
+    "url": "/countries/index.html"
+  }
+];
